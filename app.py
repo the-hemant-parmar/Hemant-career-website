1fba3fbc4f55c9af16ab41ce181ef73241f77eef
Hemant Parmar <122034890+the-hemant-parmar@users.noreply.github.com>
2023-04-08 13:48:51 +0000

    Added static files, dyanmic data and API route

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Skills = [{
+  'id': 1,
+  'title': 'Python',
+  'source': 'School, Youtube, projects',
+  'proficiency': 'Intermediate',
+}, {
+  'id': 2,
+  'title': 'Web dev',
+  'source': 'Youtube, projects',
+  'proficiency': 'Intermediate',
+}, {
+  'id': 3,
+  'title': 'mySQL',
+  'source': 'School, Youtube, projects',
+  'proficiency': 'Begginer',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "Sabhi Mandali sadasyo ko Ram Ram"
+  return render_template("home.html", skills=Skills)
+
+
+@app.route("/api/skills")
+def list_skills():
+  return jsonify(Skills)
 
 
 if __name__ == "__main__":
